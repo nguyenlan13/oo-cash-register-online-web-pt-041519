cd83670a65ffc5c13810ae9e7cc3ef818ece280b
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-31 11:44:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,6 +23,7 @@ class CashRegister
   def apply_discount
     if discount !=0
       self.total = (total*((100.00 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $800."
   end
   
   def items
