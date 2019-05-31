a35e05da4552167cde1e212fec4ce88a7412ddda
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-31 11:36:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
-
+class CashRegister
+  
+  attr_accessor :discount, :total
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @stuff = []
+  end
+  
+  def total
+    @total
+  end
+    
+  def add_item(title, price, quantity=1)
+    @total += price*quantity
+    quantity.times do
+      @stuff << title
+    end
+  end
+    
+  def apply_discount
+    @total*discount.to_f
+  end
+  
+  def items
+    @stuff
+  end
+  
+  def void_last_transaction
+    
+  end
+end
